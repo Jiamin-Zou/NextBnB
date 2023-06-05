1863f3b80e6f61e1af044aabbc48ae9a9bca3c2e
Jiamin-Zou <jiaminz622@gmail.com>
2023-06-05 09:25:40 -0400

    Added static_pages controller, ran build

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :create, :update, :destroy]
     resources :listings, only: [:index, :show] do
       resources :reservations, only: [:create]
     end
-
+    
     resources :reservations, only: [:show, :update, :destroy]
     resource :session, only: [:create, :show, :destroy]
   end
+  get "*path", to: "static_pages#frontend_index"
 end
