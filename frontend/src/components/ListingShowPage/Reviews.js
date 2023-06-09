74b4b681013ad1a26b5bcc3c75168e04bb373de5
Jiamin-Zou <jiaminz622@gmail.com>
2023-06-09 02:02:32 -0400

    Added user reviews to listing show

--- frontend/src/components/ListingShowPage/Reviews.js
+++ frontend/src/components/ListingShowPage/Reviews.js
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchListingReviews, getListingReviews } from "../../store/reviews";
-import "./Review.css"
+import { ReactComponent as Star } from "../../assets/images/star.svg";
+import "./Review.css";
+import { randomRGB } from "../../util/util";
 
 const Reviews = ({ listing }) => {
   const dispatch = useDispatch();
@@ -25,10 +27,15 @@ const Reviews = ({ listing }) => {
   const ratingFields = ratingTitle.map((rating) => {
     return (
       <div className="rating-field-item" key={rating}>
-        <div className="rating-field-left field-name">{rating.charAt(0).toUpperCase() + rating.slice(1)}</div>
+        <div className="rating-field-left field-name">
+          {rating.charAt(0).toUpperCase() + rating.slice(1)}
+        </div>
         <div className="rating-field-right">
           <div className="rating-bar outter">
-            <div className="rating-bar inner" style={{ width: `calc(${listing.ratings[rating]/5} * 100%)`}}></div>
+            <div
+              className="rating-bar inner"
+              style={{ width: `calc(${listing.ratings[rating] / 5} * 100%)` }}
+            ></div>
           </div>
           <div className="rating-field-average">{listing.ratings[rating]}</div>
         </div>
@@ -36,11 +43,43 @@ const Reviews = ({ listing }) => {
     );
   });
 
-  const reviewsCards = listingReviews.map((review) => {})
+  const reviewCards = listingReviews.map((review) => {
+    return (
+      <div className="review-card-item" key={review.id}>
+        <div className="review-user-field">
+          <div className="review-user-left">
+            <i className="fa-regular fa-circle-user" style={{color: randomRGB()}}></i>
+          </div>
+          <div className="review-user-right">
+            <div className="review-user-name">{review.reviewer}</div>
+            <div className="review-user-rating">{review.overallRating}</div>
+          </div>
+        </div>
+        <div className="review-body-field">
+          <div>{review.body}</div>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="listing-review-container">
+      <div className="listing-review-header">
+        <div className="listing-rating">
+          <div className="review-star">
+            <Star />
+          </div>
+          <div className="listing-rating-score">
+            {listing.ratings.overallRating}
+          </div>
+        </div>
+        <span className="separator">&#x2022;</span>
+        <div className="listing-num-reviews">
+          <span>{listing.numReviews} reviews</span>
+        </div>
+      </div>
       <div className="listing-average-ratings">{ratingFields}</div>
+      <div className="listing-review-cards">{reviewCards}</div>
     </div>
   );
 };
