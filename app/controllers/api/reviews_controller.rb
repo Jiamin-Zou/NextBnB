30c69ff971539a6c75b7476320a325c0073445ec
Jiamin-Zou <jiaminz622@gmail.com>
2023-06-21 17:38:00 -0400

    tested update request in frontend console, backend update working

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,58 +1,70 @@
 class Api::ReviewsController < ApplicationController
+  before_action :require_logged_in, only: [:create, :index, :show, :update, :destroy]
+  before_action :set_review, only: [:show, :update, :destroy]
 
-    before_action :require_logged_in, only: [:create, :index, :show, :update, :destroy]
-    before_action :set_review, only: [:show, :update, :destroy]
+  wrap_parameters include: Reservation.attribute_names
 
-    wrap_parameters include: Reservation.attribute_names
+  def index
+    @listing = Listing.find_by(id: params[:listing_id])
+    @reviews = @listing.reviews.includes(:reviewer)
+    render :index
+  end
 
-    def index
-        @listing = Listing.find_by(id: params[:listing_id])
-        @reviews = @listing.reviews.includes(:reviewer)
-        render :index
+  def create
+    reservation = Reservation.find_by(id: params[:reservation_id])
+    if (reservation)
+      @review = Review.new(review_params)
+      @review.reviewer_id = current_user.id
+      if (reservation.save)
+        render :show
+      else
+        render json: { errors: @review.errors.full_messages }, status: 422
+      end
+    else
+      render json: { errors: ["Attemping to review for non-existing reservation"] }
     end
+  end
 
-    def create
-        reservation = Reservation.find_by(id: params[:reservation_id])
-        if (reservation)
-            @review = Review.new(review_params)
-            @review.reviewer_id = current_user.id
-            if (reservation.save)
-                render :show
-            else
-                render json: { errors: @review.errors.full_messages }, status: 422
-            end
+  def show
+    if @review
+      render :show
+    else
+      render json: { errors: ["Review could not be found"] }, status: 404
+    end
+  end
+
+  def update
+    if @review
+      if (@review.reviewer_id == current_user.id)
+        if (@review.update(review_params))
+          render :show
         else
-            render json: { errors: ["Attemping to review for non-existing reservation"]}
+          render json: { errors: @review.errors.full_messages }, status: 422
         end
-
+      else
+        render json: { errors: ["Not allowed. That review was not posted by you."] }
+      end
+    else
+      render json: { errors: ["Review could not be found"] }, status: 404
     end
+  end
 
-    def show
-        debugger
-        if @review
-            render :show
-        else
-            render json: { errors: ["Review could not be found"] }, status: 404
-        end
+  def destroy
+    if (@review.reviewer_id == current_user.id)
+      @review.destroy
+      head :no_content
+    else
+      render json: { errors: ["Not allowed. That review was not posted by you."] }
     end
+  end
 
-    def update
+  private
 
-    end
+  def set_review
+    @review = Review.find_by(id: params[:id])
+  end
 
-    def destroy
-
-    end
-
-    private
-
-    def set_review
-        @review = Review.find_by(id: params[:id])
-    end
-
-    def review_params
-        params.require(review).permit(:reservation_id, :cleanliness, :accuracy, :value, :communication, :check_in, :location, :body)
-    end
-
-
+  def review_params
+    params.require(:review).permit(:reservation_id, :cleanliness, :accuracy, :value, :communication, :check_in, :location, :body)
+  end
 end
